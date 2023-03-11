cc7613ea966972e95098c728ba26a44eda1d0331
Sarah de Moraes <sarah.batista@germinare.org.br>
2023-03-11 03:03:13 -0300

    Corrigindo o codigo

--- Cliente2.java
+++ Cliente2.java
@@ -1,11 +1,8 @@
 public class Cliente2 {
-<<<<<<< HEAD
     private String tipoConta;
     private int rg;
-=======
     private int numeroIdentificacao;
     private int rgGeovanna;
->>>>>>> classe-cliente2
     private int cpf;
     private int agencia;
     private double saldo;
